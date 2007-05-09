8f94b352cb9d2e7424fdfc33a8f20a14b1c0b3d7
lajnold <lajnold@712a1f97-fb38-4509-82a9-c8a0c282b14c>
2007-05-09 20:33:05 +0000

    Made things private in IRCBot.
    git-svn-id: svn+ssh://white/home/lajnold/svn/IRCBot@20 712a1f97-fb38-4509-82a9-c8a0c282b14c

--- IRCBot.h
+++ IRCBot.h
@@ -19,10 +19,6 @@ namespace IRC
 		
 		typedef std::vector<std::string> message_list_type;
 		
-	public:
-
-		IRCBot(const std::string &address, const unsigned int port, const std::string &channel, const char command_char);
-		
 		void send(std::string message);
 		
 		void split_string(message_list_type &out, const std::string &input, const std::string &delimiter);
@@ -33,6 +29,9 @@ namespace IRC
 		std::string get_user(const message_list_type &input);
 		std::string get_time_string();
 		std::string make_voice_action(const std::string &action, const message_list_type &input);
+		
+		void parse_irc_message(const std::string &input);
+		void parse_data(const std::string &input);
 		
 		bool is_privmsg(const message_list_type &input);
 		bool is_privmsg_in_channel(const message_list_type &input);
@@ -50,8 +49,10 @@ namespace IRC
 		void update_user_word_count(const message_list_type &input);
 		unsigned int get_user_word_count(const message_list_type &input);
 		
-		void parse_irc_message(const std::string &input);
-		void parse_data(const std::string &input);
+	public:
+		
+		IRCBot(const std::string &address, const unsigned int port, const std::string &channel, const char command_char);
+		
 		void run();
 	};
 }

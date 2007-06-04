9b62e9e0222e1fc1e2dbad3f9403bcfd68eaf13b
lajnold <lajnold@712a1f97-fb38-4509-82a9-c8a0c282b14c>
2007-06-04 23:44:23 +0000

    Added base for commandline parsing.
    git-svn-id: svn+ssh://white/home/lajnold/svn/IRCBot@33 712a1f97-fb38-4509-82a9-c8a0c282b14c

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
+#include <string>
+
+#include <boost/program_options.hpp>
+
 #include "IRCBot.h"
+
+struct BotOptions
+{
+	std::string address;
+	unsigned short port;
+	
+	std::string nickname;
+	std::string channel;
+	
+	char command_char;
+	
+	BotOptions() : address("localhost"), port(9997), nickname("Amon"), channel("#qwer"), command_char('!') { };
+};
+
+void parse_commandline(BotOptions &options)
+{
+	
+}
 
 int main(int argc, char *argv[])
 {
-	IRC::IRCBot bot("127.0.0.1", 9997, "Amon", "#qwer", '!');
+	BotOptions options;
+	
+	IRC::IRCBot bot(options.address, options.port, options.nickname, options.channel, options.command_char);
 	bot.run();
 }

309acca31af8d1a612bdb0c0dd491ee44e493415
lajnold <lajnold@712a1f97-fb38-4509-82a9-c8a0c282b14c>
2007-06-05 23:59:31 +0000

    Added command line parsing.
    git-svn-id: svn+ssh://white/home/lajnold/svn/IRCBot@34 712a1f97-fb38-4509-82a9-c8a0c282b14c

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 
 #include <boost/program_options.hpp>
 
@@ -13,18 +14,51 @@ struct BotOptions
 	std::string channel;
 	
 	char command_char;
-	
-	BotOptions() : address("localhost"), port(9997), nickname("Amon"), channel("#qwer"), command_char('!') { };
 };
 
-void parse_commandline(BotOptions &options)
+bool parse_commandline(int argc, char *argv[], BotOptions &options)
 {
+	namespace po = boost::program_options;
 	
+	po::options_description desc;
+	desc.add_options()
+		("help", "show options")
+		("host", po::value<std::string>(&options.address), "the address of the IRC server")
+		("port", po::value<unsigned short>(&options.port)->default_value(6667), "the port to connect to")
+		("nick", po::value<std::string>(&options.nickname)->default_value("Amon"), "the nickname the bot should use")
+		("channel", po::value<std::string>(&options.channel), "the channel the bot should be in")
+		("commandchar", po::value<char>(&options.command_char)->default_value('!'), "the character the bot should react on");
+	
+	po::variables_map vm;
+	
+	try
+	{
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+	}
+	catch(const po::invalid_command_line_syntax &e)
+	{
+		std::cout << e.what() << std::endl << std::endl;
+		std::cout << desc << std::endl;
+		
+		return false;
+	}
+	
+	if(vm.count("help") > 0)
+	{
+		std::cout << desc << std::endl;
+		return false;
+	}
+	
+	return true;
 }
 
 int main(int argc, char *argv[])
 {
 	BotOptions options;
+	
+	if(!parse_commandline(argc, argv, options))
+		return 0;
 	
 	IRC::IRCBot bot(options.address, options.port, options.nickname, options.channel, options.command_char);
 	bot.run();
